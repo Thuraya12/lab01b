7a3c4c97da93254fe3cbe5376ab84012a74fcf0b
Thuraya12 <isthurayaalhowari@yahoo.com>
2021-02-07 17:00:06 +0200

    lab01b

---
+++ app.js
@@ -0,0 +1,14 @@
+
+let favLeague= prompt ('What\'s your favorit football league in Europe?');
+console.log(favLeague);
+
+let favTeam=prompt ('Waht\'s your favorite european football team?');
+console.log(favTeam);
+
+let favChampionship=prompt ('What\'s your favorite football championship title?');
+console.log(favChampionship);
+
+let favPlayer=prompt ('What\'s your favorite football player?');
+console.log(favPlayer);
+
+alert('Hello, you like '+ favPlayer +' , ' +favTeam + ' ,' +favLeague+' , ' +favChampionship + ' ,' + ' Football is Fun!');
